6198606e1abe8c01d730e8abe1269a24c3f81de2
cnadaud <cnadaud@arp36.local>
2024-12-15 18:08:13 +0100

    Implement searching

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
   sortOrder: string;
+  searchTxt: string;
 }
 
 function App() {
@@ -28,7 +29,9 @@ function App() {
       }}
     >
       <GridItem area="nav">
-        <NavBar></NavBar>
+        <NavBar
+          onSearch={(searchTxt) => setGameQuery({ ...gameQuery, searchTxt })}
+        ></NavBar>
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
