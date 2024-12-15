b632b4c5d6b8419743dc3c89e811a1c54178ce74
cnadaud <cnadaud@arp36.local>
2024-12-15 19:15:16 +0100

    Set up for production

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,5 +1,5 @@
 import { HStack, Icon } from "@chakra-ui/react";
-import { Platform } from "../Hooks/useGames";
+import { Platform } from "../Hooks/usePlatform";
 import {
   FaWindows,
   FaPlaystation,
